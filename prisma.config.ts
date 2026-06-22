04b4e333f5c607d169605942713154eb42cc5a18
Justice <ezejustice112@gmail.com>
2026-06-22 17:58:23 +0100

    fix: remove deprecated earlyaccess property from prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, env } from '@prisma/config';
 
 export default defineConfig({
-  earlyAccess: true,
   schema: 'prisma/schema.prisma',
   datasource: {
     url: env('DATABASE_URL'),
